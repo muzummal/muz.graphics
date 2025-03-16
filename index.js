abf2d9f11a65c2ab9ed5d94b21339810d34dc682
Muz <85709118+muzummal@users.noreply.github.com>
2025-03-16 16:10:18 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var text = ["design wizard", "technical artist", "problem solver"];
+var counter = 0;
+var elem = document.getElementById("adj-noun");
+var inst = setInterval(changeTitle, 5000);
+
+
+
+function changeTitle() {
+    elem.innerHTML = text[counter];
+    counter++;
+    if (counter >= text.length) {
+        counter =0;
+    }
+}
